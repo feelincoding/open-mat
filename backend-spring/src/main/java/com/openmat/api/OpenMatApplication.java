ca5353d3e3e40be7800d7570171245f056dec9ce
feelincoding <feelincoding@gmail.com>
2026-01-29 11:26:53 +0900

    test

--- backend-spring/src/main/java/com/openmat/api/OpenMatApplication.java
+++ backend-spring/src/main/java/com/openmat/api/OpenMatApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendSpringApplication {
+public class OpenMatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendSpringApplication.class, args);
+		SpringApplication.run(OpenMatApplication.class, args);
 	}
 
 }
